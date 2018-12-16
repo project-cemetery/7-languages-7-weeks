463143a441d3bf8bac464a96783025e7806155cb
Igor Kamyshev <igor@kamyshev.me>
2018-12-16 12:53:17 +0300

    migrate to lib from cutom solution for permutation

--- 3-Prolog/day-2/03-sorted.pl
+++ 3-Prolog/day-2/03-sorted.pl
@@ -1,11 +1,7 @@
-takeout(Head, [Head|Tail], Tail).  
-takeout(Head1, [Head2|Tail2], [Head2|Result]) :- takeout(Head1, Tail2, Result).
-
-perm([Head|Tail], Result) :- perm(Tail, Permutaed), takeout(Head, Result, Permutaed).  
-perm([],[]).
+:- use_module(library(lists)).
 
 is_sorted([]).
 is_sorted([_]).
 is_sorted([Head1|[Head2|Tail]]) :- Head1 =< Head2, is_sorted([Head2|Tail]).
 
-sorted(List, Sorted) :- perm(List, Sorted), is_sorted(Sorted).
+sorted(List, Sorted) :- permutation(List, Sorted), is_sorted(Sorted).
